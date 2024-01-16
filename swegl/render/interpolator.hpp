f1ada0d285bd096fd80c242687fa8764fa52390a
gabriel <gabriel.bizzotto@gmail.com>
2024-01-15 23:02:34 -0300

    Deleting old code.

--- swegl/render/interpolator.hpp
+++ swegl/render/interpolator.hpp
@@ -6,45 +6,7 @@
 
 namespace swegl
 {
- struct interpolator_t_old
- {
-	freon::Matrix<float,1,2> topalpha, topstep;
-	freon::Matrix<float,1,2> ualpha;
-	float bottomalpha, bottomstep;
 
-	inline void Init(const float dist, const float z1, const float z2)
-	{
-		float alphastep = 1.0f / dist; // dist always != 0
-		bottomalpha = 1.0f / z1;
-		float invz2 = 1.0f / z2;
-		ualpha[0][0] = 0.0f;
-		ualpha[0][1] = z1;
-		topalpha = ualpha;
-		topalpha *= bottomalpha;
-		topstep[0][0] = (1.0f * invz2 - topalpha[0][0]) * alphastep;
-		topstep[0][1] = (z2   * invz2 - topalpha[0][1]) * alphastep;
-		bottomstep = (invz2-bottomalpha)*alphastep;
-	}
-	inline void DisplaceStartingPoint(const float & move) {
-		topalpha += topstep * move;
-		bottomalpha += bottomstep * move;
-		div(ualpha, topalpha, bottomalpha);
-	}
-	inline void Step() {
-		topalpha += topstep;
-		bottomalpha += bottomstep;
-		div(ualpha, topalpha, bottomalpha);
-	}
-
-	inline float progress() const { return ualpha[0][0]; }
-	inline float z() const { return ualpha[0][1]; }
-
-	inline static void div(freon::Matrix<float,1,2> & a, const freon::Matrix<float,1,2> & b, float c)
-	{
-		a[0][0] = b[0][0] / c;
-		a[0][1] = b[0][1] / c;
-	};
-};
 struct interpolator_progress
 {
 	float topalpha, ualpha;
@@ -86,7 +48,6 @@ struct interpolator_progress
 		a[0][1] = b[0][1] / c;
 	};
 };
-
 
 template<size_t N>
 struct interpolator_g
