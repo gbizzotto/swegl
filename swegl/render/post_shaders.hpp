092570ecf81b4d75d44f4d799f080018ca63fff2
gabriel <gabriel.bizzotto@gmail.com>
2024-01-21 08:37:41 -0300

    Optimization in bilinear filtering.

--- swegl/render/post_shaders.hpp
+++ swegl/render/post_shaders.hpp
@@ -31,11 +31,13 @@ struct post_shader_depth_box : public post_shader_t
 		for (int y=0 ; y<vp.m_h ; y++)
 			for (int x=0 ; x<vp.m_w ; x++)
 			{
+				__builtin_prefetch(&frame_buffer[(std::max(0,y-5)+vp.m_y)*vp.m_screen->pitch/4 + std::max(0,x-5)+vp.m_x]);
 				float z = vp.zbuffer()[y*vp.m_w + x];
 				int blur = remap_clipped(0.0f, focal_depth, 0.0f, 5.0f, abs(focal_distance-z));
 				int b=0, g=0, r=0;
 				int count = 0;
 				for (int j=std::max(0,y-blur) ; j<std::min(vp.m_h,y+blur) ; j++)
+				{
 					for (int i=std::max(0,x-blur) ; i<std::min(vp.m_w,x+blur) ; i++)
 					{
 						count++;
@@ -44,6 +46,7 @@ struct post_shader_depth_box : public post_shader_t
 						g += p.o.g;
 						r += p.o.r;
 					}
+				}
 				if (count)
 					frame_buffer[(y+vp.m_y)*vp.m_screen->pitch/4 + x+vp.m_x] = pixel_colors(b/count,g/count,r/count);
 			}
