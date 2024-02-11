126932c21f3e0e0bc5c34ca4dd561d7e7ef2a7c5
gabriel <gabriel.bizzotto@gmail.com>
2024-02-10 22:15:00 -0300

    New class screen_t to wrap homemade and SDL_Surface screens, with zbuffer.

---
+++ src/test_screen.cpp
@@ -0,0 +1,58 @@
+
+#include "headers.hpp"
+
+#include <swegl/misc/sdl.hpp>
+#include <swegl/misc/screen.hpp>
+
+int main(int argc, char *argv[])
+{
+	swegl::sdl_t sdl(400, 1600, 800, 600, "test_1");
+	SDL_Surface * surface = sdl.surface;
+
+	swegl::screen_t screen(800, 600);
+	screen.for_each([](float & z, swegl::pixel_colors & c, int x, int y)
+		{
+			c = swegl::pixel_colors(128,0,0,255);
+		});
+
+	swegl::screen_t screen_2(sdl.surface);
+	screen_2.for_each([](float & z, swegl::pixel_colors & c, int x, int y)
+		{
+			c = swegl::pixel_colors(0,0,128,255);
+		});
+
+	for (swegl::screen_t::iterator it_screen = screen.begin()
+		                          ,it_end    = screen.end()
+		                          ,it_screen_2 = screen_2.begin()
+		;it_screen != it_end
+		;++it_screen,++it_screen_2)
+	{
+		*it_screen_2 = *it_screen_2 + *it_screen;
+	}
+
+	for (auto it=screen_2.iterator_at_line(100), end=screen_2.iterator_at_line(200)
+		;it!=end
+		;it++)
+	{
+		*it = swegl::pixel_colors(0,128,128,255);
+	}
+
+	
+	sdl.update_frame();
+
+	for (SDL_Event event;;)
+	{
+		while (SDL_PollEvent(&event))
+			switch (event.type)
+			{
+				case SDL_KEYDOWN:
+					if (event.key.keysym.sym == SDLK_ESCAPE)
+						return -1;
+					break;
+				default:
+					break;
+			}
+	}
+
+	return 0;
+}
---
+++ swegl/misc/screen.hpp
@@ -0,0 +1,135 @@
+
+#pragma once
+
+#include <memory>
+#include <SDL2/SDL.h>
+#include <swegl/render/colors.hpp>
+
+namespace swegl
+{
+
+class screen_t
+{
+	float        * zbuffer;
+	pixel_colors * pixels;
+	int w_, h_;
+	int x_gap;
+	bool owns_zbuffer;
+	bool owns_pixels ;
+
+public:
+	inline screen_t(int w, int h)
+		: zbuffer(new float[w*h])
+		, pixels (new pixel_colors[w*h])
+		, w_(w)
+		, h_(h)
+		, x_gap(0)
+		, owns_zbuffer(true)
+		, owns_pixels(true)
+	{}
+	inline screen_t(SDL_Surface * sdl_surface)
+		: zbuffer(new float[sdl_surface->w*sdl_surface->h])
+		, pixels ((pixel_colors*)sdl_surface->pixels)
+		, w_(sdl_surface->w)
+		, h_(sdl_surface->h)
+		, x_gap(sdl_surface->pitch/sdl_surface->format->BytesPerPixel - sdl_surface->w)
+		, owns_zbuffer(true)
+		, owns_pixels(false)
+	{}
+	inline ~screen_t()
+	{
+		if (owns_zbuffer)
+			delete[] zbuffer;
+		if (owns_pixels )
+			delete[] pixels;
+	}
+
+	int w() const { return w_; }
+	int h() const { return h_; }
+	int gap() const { return x_gap; }
+
+	template<typename F>
+	void for_each(F && f)
+	{
+		float * z = zbuffer;
+		int x = 0;
+		int y = 0;
+		for (pixel_colors *p=pixels, *p_end=pixels+((w_+x_gap)*h_) ; p < p_end ; p += x_gap, ++y)
+			for (pixel_colors *line_end=p+w_ ; p<line_end ; ++p, ++z, ++x)
+				f(*z, *p, x, y);
+	}
+
+	friend class iterator;
+	class iterator
+	{
+	public:
+		float * z;
+		int x;
+		int y;
+		int y_end;
+		int x_gap;
+		pixel_colors * p;
+		pixel_colors * p_line_end;
+
+		inline iterator(const screen_t & s, size_t pixel_idx)
+			: z( & s.zbuffer[pixel_idx])
+			, x(pixel_idx % s.w())
+			, y(pixel_idx / s.w())
+			, y_end(s.h())
+			, x_gap(s.x_gap)
+			, p         ( & s.pixels[(pixel_idx / s.w()    ) * (s.w()+s.gap()) + pixel_idx % s.w()])
+			, p_line_end( & s.pixels[(pixel_idx / s.w() + 1) * (s.w()+s.gap())                    ])
+		{
+			assert(pixel_idx <= s.w() * s.h());
+		}
+
+		inline iterator() = default;
+		inline iterator(const iterator & ) = default;
+		inline bool operator==(const iterator & other) { return z == other.z; }
+		inline bool operator!=(const iterator & other) { return z != other.z; }
+		inline        pixel_colors & operator *()       { return *p; }
+		inline const  pixel_colors & operator *() const { return *p; }
+		inline        pixel_colors * operator->()       { return  p; }
+		inline const  pixel_colors * operator->() const { return  p; }
+		inline iterator& operator++() //prefix increment
+		{
+			if (y == y_end)
+				return *this;
+			++z;
+			++p;
+			if (p == p_line_end)
+			{
+				p += x_gap;
+				y++;
+				x = 0;
+			}
+			else
+			{
+				x++;
+			}
+			return *this;
+		}
+		inline iterator operator++(int) // postfix increment
+		{
+			auto old = *this;
+			++(*this);
+			return old;
+		}
+	};
+
+	inline iterator begin()
+	{
+		return iterator(*this, 0);
+	}
+	inline iterator end()
+	{
+		return iterator(*this, w()*h());
+	}
+
+	inline iterator iterator_at_line(int y)
+	{
+		return iterator(*this, y * w());
+	}
+};
+	
+} // namespace
